067f6cf8e1dd041a515d600e67f1665e3b818361
carlabeltran <carlabeltran14@icloud.com>
2020-02-21 21:44:53 -0600

    added todays date, weekday, and divider

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 //TESTING TO SEE IF JAVASCRIPT FILE & HTML ARE PROPERLY LINKED
 console.log("HTML & JAVA ARE PROPERLY LINKED!!")
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
+
+/////////////////////////////////////////////////////////////////////
+//VARIABLES & DOM ELEMENTS
+/////////////////////////////////////////////////////////////////////
+//DISPLAYS THE DAY
+const todaysDate = moment().format("MMMM Do, YYYY")
+console.log("todaysDate:", todaysDate);
+
+//DISPLAYS THE CURRENT TIME AM/PM
+const currentTime = moment().format("h:mm a");
+console.log("currentTime:", currentTime);
+
+//DISPLAYS THE CURRENT HOUR AM / PM
+const currentHour = moment().format("h A");
+console.log("currentHour:", currentHour);
+
+//DISPLAYS THE CURRENT HOUR AM / PM
+const currentWeekday = moment().format("dddd");
+console.log("currentWeekday:", currentWeekday);
 // const currentWeatherStatus;
+
+
+$(".time").text(currentTime);
+
+$(".weekday").text(currentWeekday);
+
+$(".date").text(todaysDate);
+
+
+
