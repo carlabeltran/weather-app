e324f51c11098d4729e1aa4ed507eea58d3f6a21
carlabeltran <carlabeltran14@icloud.com>
2020-03-02 18:28:47 -0600

    took the text shadow off of the cards and outlined the cards

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //TESTING TO SEE IF JAVASCRIPT FILE & HTML ARE PROPERLY LINKED
-console.log("HTML & JAVA ARE PROPERLY LINKED!!")
+console.log("HTML & JAVA ARE PROPERLY LINKED!!");
 
 //CALLING MY TIME NOW FUNCTION TO ACTION
 timeNow();
@@ -14,18 +14,19 @@ console.log("updateTime:", updateTime);
 
 //FUNCTION TO UPDATE MY TIME HOURS AND MINUTES
 function timeNow() {
-    const time = new Date();
-    console.log('time:', time)
+  const time = new Date();
+  console.log("time:", time);
 
-    const hours = time.getHours();
-    console.log("hours:", hours);
-    
-    const minutes = time.getMinutes();
-    console.log("minutes:", minutes);
+  const hours = time.getHours();
+  console.log("hours:", hours);
 
-    const ampm = moment().format("A");
+  const minutes = time.getMinutes();
+  console.log("minutes:", minutes);
 
-    document.getElementById("time").innerText = hours + ":" + minutes + " " + ampm ;
+  const ampm = moment().format("A");
+
+  document.getElementById("time").innerText =
+    hours + ":" + minutes + " " + ampm;
 }
 
 /////////////////////////////////////////////////////////////////////
@@ -47,7 +48,6 @@ console.log("currentHour:", currentHour);
 const currentWeekday = moment().format("dddd");
 console.log("currentWeekday:", currentWeekday);
 
-
 // const currentWeatherStatus;
 
 //DISPLAYS THE CURRENT TIME
@@ -59,87 +59,87 @@ $(".weekday").text(currentWeekday);
 //DISPLAYS THE CURRENT DATE
 $(".date").text(todaysDate);
 
-
 ////////////////////////////////////////////////////////////////////
 //CREATE A FUNCTION TO FORMAT HOURS && CHANGES THE HOUR DISPLAY FROM MIILTARY TIME TO REGULAR TIME
 /////////////////////////////////////////////////////////////////////
 function formatAMPM(hour) {
-    console.log("format AMPM is working!!");
-    
-    const ampm = hour >= 12 ? "PM" : "AM";
-  //variable ampm equals hours greater than or equal to 
+  console.log("format AMPM is working!!");
+
+  const ampm = hour >= 12 ? "PM" : "AM";
+  //variable ampm equals hours greater than or equal to
   //variable ampm is storing greater than or equal to 12 ternary operator
   //>= : greater than or equal to
   // ?: ternary operator
-                            
-    hour = hour % 12;
-  //hour equal hours 
+
+  hour = hour % 12;
+  //hour equal hours
   //% :   Modulus (Division Remainder)
-    
-    hour = hour ? hour : 12;
-  //HOURS EQUALS HOURS 
-    
-    return hour + " " + ampm;
-};
+
+  hour = hour ? hour : 12;
+  //HOURS EQUALS HOURS
+
+  return hour + " " + ampm;
+}
 //INITIALIZE FORMATAMPM(HOURS)
 formatAMPM();
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
 
 function displayWeatherTimeIcons() {
+  //THIS VARIBALE STORS THE HOUR ID
+  const hourId = parseInt(moment().hour());
+  console.log("hourId:", hourId);
 
-    //THIS VARIBALE STORS THE HOUR ID
-    const hourId = parseInt(moment().hour());
-    console.log("hourId:", hourId);
+  //HOUR FOR LOOP
+  for (let hour = 0; hour <= 24; hour++) {
+    //variable hour = 9am; less than 6pm; hour increase each time)
 
-    //HOUR FOR LOOP 
-    for (let hour = 0; hour <= 24; hour++) {
-        //variable hour = 9am; less than 6pm; hour increase each time)
+    const hourEl = moment().hour(hour);
+    console.log("hourEl:", hourEl);
 
-        const hourEl = moment().hour(hour);
-        console.log("hourEl:", hourEl)
+    const hourIndex = parseInt(hourEl.format("h"));
+    console.log("hourIndex:", hourIndex);
 
-        const hourIndex = parseInt(hourEl.format("h"));
-        console.log("hourIndex:", hourIndex);
+    //CREATE A ROW TO WRAP EVERYTHING IN
+    const card = $('<div class="card"></div>');
 
-        //CREATE A ROW TO WRAP EVERYTHING IN
-        const card = $('<div class="card"></div>');
+    const cardHeader = $('<div class="cardTitle"></div>');
 
-        const cardHeader = $('<div class="cardTitle"></div>');
+    const hourSpan = $(
+      '<span class="hour" style="color:#ffffff;font-size:15px;font-weight: bold;font-family:Poppins, sans-serif;">' +
+        formatAMPM(hour) +
+        "</span>"
+    );
+    console.log("hour:", hour);
 
-        const hourSpan = $('<span class="hour" style="color:#ffffff;font-size:15px;font-weight: bold;font-family:Poppins, sans-serif;">' + formatAMPM(hour) + "</span>");
-        console.log("hour:", hour);
+    cardHeader.append(hourSpan);
 
-        cardHeader.append(hourSpan);
+    const cardBody = $('<div class="box"></div>');
 
-        const cardBody = $('<div class="box"></div>');
+    const iconSpan = $('<span class="icon"></span>');
 
-        const iconSpan = $('<span class="icon"></span>');
+    const iconId = "icon-temp-" + hour;
+    console.log("iconId:", iconId);
 
-        const iconId = "icon-temp-" + hour;
-        console.log("iconId:", iconId);
+    iconSpan.attr("id", iconId);
+    console.log("iconSpan:", iconSpan);
 
-        iconSpan.attr("id", iconId);
-        console.log("iconSpan:", iconSpan);
+    cardBody.append(iconSpan);
 
-        cardBody.append(iconSpan);
+    const cardFooter = $('<div class="cardFooter"></div>');
 
-        const cardFooter = $('<div class="cardFooter"></div>');
+    const timeTempSpan = $('<span class="temp"></span>');
+    // console.log("temp:", temp);
 
-        const timeTempSpan = $('<span class="temp"></span>');
-        // console.log("temp:", temp);
+    timeTempSpan.attr("data-iconspan", hour);
+    console.log(cardFooter);
 
-        timeTempSpan.attr("data-iconspan", hour);
-        console.log(cardFooter);
+    cardFooter.append(timeTempSpan);
 
-        cardFooter.append(timeTempSpan);
+    card.append(cardHeader);
+    card.append(cardBody);
+    card.append(cardFooter);
 
-        card.append(cardHeader);
-        card.append(cardBody);
-        card.append(cardFooter);
-
-        $(".horizontal-scroll").append(card);
-    };
-
-
-};
+    $(".horizontal-scroll").append(card);
+  }
+}
