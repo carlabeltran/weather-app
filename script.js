86f247b3e45ae8a60a466f6e535486cae546b8d6
carlabeltran <carlabeltran14@icloud.com>
2020-03-14 17:26:27 -0500

    weather app is fully functional need to set up forecast, uv, & local storage

--- script.js
+++ script.js
@@ -1,151 +1,276 @@
 //TESTING TO SEE IF JAVASCRIPT FILE & HTML ARE PROPERLY LINKED
 console.log("HTML & JAVA ARE PROPERLY LINKED!!");
 
+/////////////////////////////////////////////////////////////////////
+//VARIABLES & DOM ELEMENTS
+/////////////////////////////////////////////////////////////////////
+//DISPLAYS THE DAY
+const todaysDate = moment().format("MMMM D, YYYY");
+// console.log("todaysDate:", todaysDate);
+
+//DISPLAYS THE CURRENT TIME AM/PM
+const currentTime = moment().format("h:mm a");
+// console.log("currentTime:", currentTime);
+
+//DISPLAYS THE CURRENT HOUR AM / PM
+const currentHour = moment().format("h A");
+// console.log("currentHour:", currentHour);
+
+//DISPLAYS THE CURRENT HOUR AM / PM
+const currentWeekday = moment().format("dddd");
+// console.log("currentWeekday:", currentWeekday);
+
+//UPDATES TIME
+const updateTime = setInterval(timeNow, 60000);
+// console.log("updateTime:", updateTime);
+
+//CITY
+$cityDisplay = $("#city");
+
+//DESCRIPTION
+$cityStatus = $(".description");
+
+//ICON
+$icon = $("#icon");
+
+//SEARCH BUTTON
+$searchButton = $("#searchButton");
+
+//INPUT FIELD
+$searchInput = $("#searchTxt");
+
+//WEATHER STAGE
+$weatherStage = $(".weatherStage");
+
+//TEMPERTURE
+$temp = $(".temp");
+
+//HUMIDITY
+$humid = $(".humidity");
+
+//HIGH TEMPERTURE
+$hTemp = $(".highTemp");
+
+//LOW TEMPERTURE
+$lTemp = $(".lowTemp");
+
+//WIND SPEED
+$wind = $(".wind");
+
+//FORECAST STATUS
+$forecastStatus = $("#forecastStage");
+
 //API KEY
-const key = "166a433c57516f51dfab1f7edaed8413"; 
+var apiKey = "&APPID=bb2faf18c9357f761b725ea926a3bde5";
 
-// CREATING URL NEEDED TO QUERY DATABASE
-const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=" + key;
-  
+//CURRENT WEATHER URL
+var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
-const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + key;
+//FORECAST URL
+var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
 
-const uvURL = "https://api.openweathermap.org/data/2.5/uvi?";
+//UV URL
+var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?";
+
+//STORING USER INPUT TEXT VALUE FOR API
+var queryCity = $searchInput.val();
+
+//CURRENT DATE & TIME
+var date = new Date().toLocaleDateString("en-US");
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 
-//RUNNING AJAX CALL TO OPENWEATHERMAP API
-$.ajax({
-  url: baseURL,
-  method: "GET"
-})
-  //STORE ALL RETRIEVED DATA INSIDE OBJECT RESPONSE
-  .then(function (response) {
-        
-    console.log(baseURL);
-    console.log(response);
-        
-    //TRANSFER DATA/CONTENT TO HTML
-    // CITY
-    $(".city").html(response.name + " Weather Details");
-    
-    //WIND
-    $(".wind").text("Wind Speed: " + response.wind.speed);
-        
+
+/////////////////////////////////////////////////////////////////////
+//ON CLICK SEARCH BUTTON FUNCTION
+/////////////////////////////////////////////////////////////////////
+$searchButton.on("click", function () {
+  //SET USER INPUT (CITY) IN API URL
+  queryCity = $searchInput.val();
+  //TEST VALUE
+  console.log(queryCity);
+  $.ajax({
+    url: queryUrl + queryCity + "&units=imperial" + apiKey,
+    method: "GET"
+  }).then(function(data) {
+    //VIEW DATA FROM API
+    console.log(data);
+    //////////////////////////////////
+    //STORING DATA IN VARIABLES
+    /////////////////////////////////
+    //CITY NAME
+    var cityName = data.name;
+    console.log(cityName);
+    console.log(data.name);
+
+    //TEMP FARENHEIT
+    var tempF = data.main.temp;
+    console.log(tempF);
+    console.log("Temperature (F): " + data.main.temp);
+
     //HUMIDITY
-    $(".humidity").text("Humidity: " + response.main.humidity);
-        
+    var humidity = data.main.humidity;
+    console.log(humidity)
+    console.log("Humidity: " + data.main.humidity);
+
+    //WIND SPEED
+    var windSpeed = data.wind.speed;
+    console.log(windSpeed);
+    console.log(data.wind.speed);
+
+    //WEATHER DESCRIPTION
+    var currentDescription = data.description;
+    console.log(currentDescription);
+    console.log(data.description);
+
+    //FEELS LIKE DESCRIPTION
+    var feelsLike = data.main.feels_like;
+    console.log(feelsLike);
+    console.log(data.main.feels_like);
+
+    //HIGH TEMP
+    var highTemp = data.main.temp_max;
+    console.log(highTemp);
+    
+    //LOW TEMP
+    var lowTemp = data.main.temp_min;
+    console.log(lowTemp);
+    
+    //ICON
+    var iconUrl = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+    
+    //ICON
+    var icon = $("<img>").attr("src", iconUrl);
+
+    // CITY
+    $(".city").text("<div>" + data.name +
+      ", " +
+      data.sys.country +
+      "(" +
+      date +
+      ")" +
+      +"</div>"
+    );
+    $(".icon").empty();
+    $(".icon").append(icon);
+    icon.addClass("img1");
+
+    //////////////////////////////////
+    //DISPLAYING CURRENT WEATHER STATUS BASED ON USER INPUT
+    /////////////////////////////////
+    //CITY
+    $cityDisplay.text(cityName);
+    //CITY DESCRIPTION
+    $cityStatus.text(currentDescription);
+    //ICON
+    $icon.attr("src", iconUrl);
     //TEMP
-    $(".temp").text("Temperature (F) " + response.main.temp);
-
-    console.log("Wind Speed: " + response.wind.speed);
-    console.log("Humidity: " + response.main.humidity);
-    console.log("Temperature (F): " + response.main.temp);
-
-
+    $temp.text(tempF + "F");
+    //HIGH TEMP
+    $hTemp.text(highTemp + "F");
+    //LOW TEMP
+    $lTemp.text(lowTemp + "C");
+    //HUMIDITY
+    $humid.text("Humidity: " + humidity + "%");
+    //WIND SPEED
+    $wind.text("Wind Speed: " + windSpeed + " MPH");
 
   });
+});
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 
-  //CALLING MY TIME NOW FUNCTION TO ACTION
-  timeNow();
-  console.log("timeNow:", timeNow);
+/////////////////////////////////////////////////////////////////////
+//FUNCTION TO UPDATE MY TIME HOURS AND MINUTES
+/////////////////////////////////////////////////////////////////////
+//CALLING MY TIME NOW FUNCTION TO ACTION
+timeNow();
+// console.log("timeNow:", timeNow);
 
-  displayWeatherTimeIcons();
-  /////////////////////////////////////////////////////////////////////
-  //FUNCTION TO UPDATE MY TIME HOURS AND MINUTES
-  /////////////////////////////////////////////////////////////////////
-  const updateTime = setInterval(timeNow, 60000);
-  console.log("updateTime:", updateTime);
+displayWeatherTimeIcons();
 
-  //FUNCTION TO UPDATE MY TIME HOURS AND MINUTES
-  function timeNow() {
-    const time = new Date();
-    console.log('time:', time)
+//FUNCTION TO UPDATE MY TIME HOURS AND MINUTES
+function timeNow() {
+  const time = new Date();
+  // console.log('time:', time)
 
-    const hours = time.getHours();
-    console.log("hours:", hours);
-    
-    const minutes = time.getMinutes();
-    console.log("minutes:", minutes);
+  const hours = time.getHours();
+  // console.log("hours:", hours);
 
-    const ampm = moment().format("A");
+  const minutes = time.getMinutes();
+  // console.log("minutes:", minutes);
 
-    document.getElementById("time").innerText = hours + ":" + minutes + " " + ampm;
-  }
+  const ampm = moment().format("A");
 
-  /////////////////////////////////////////////////////////////////////
-  //VARIABLES & DOM ELEMENTS
-  /////////////////////////////////////////////////////////////////////
-  //DISPLAYS THE DAY
-  const todaysDate = moment().format("MMMM D,  YYYY");
-  console.log("todaysDate:", todaysDate);
+  document.getElementById("time").innerText = hours + ":" + minutes + " " + ampm;
+}
 
-  //DISPLAYS THE CURRENT TIME AM/PM
-  const currentTime = moment().format("h:mm a");
-  console.log("currentTime:", currentTime);
+//DISPLAYS THE CURRENT TIME
+$(".time").text(currentTime);
 
-  //DISPLAYS THE CURRENT HOUR AM / PM
-  const currentHour = moment().format("h A");
-  console.log("currentHour:", currentHour);
+//DISPLAYS THE CURRENT WEEKDAY
+$(".weekday").text(currentWeekday);
 
-  //DISPLAYS THE CURRENT HOUR AM / PM
-  const currentWeekday = moment().format("dddd");
-  console.log("currentWeekday:", currentWeekday);
-
-  // const currentWeatherStatus;
-
-  //DISPLAYS THE CURRENT TIME
-  $(".time").text(currentTime);
-
-  //DISPLAYS THE CURRENT WEEKDAY
-  $(".weekday").text(currentWeekday);
-
-  //DISPLAYS THE CURRENT DATE
-  $(".date").text(todaysDate);
+//DISPLAYS THE CURRENT DATE
+$(".date").text(todaysDate);
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 
-  ////////////////////////////////////////////////////////////////////
-  //CREATE A FUNCTION TO FORMAT HOURS && CHANGES THE HOUR DISPLAY FROM MIILTARY TIME TO REGULAR TIME
-  /////////////////////////////////////////////////////////////////////
-  function formatAMPM(hour) {
-    console.log("format AMPM is working!!");
-    
-    const ampm = hour >= 12 ? "PM" : "AM";
-    hour = hour % 12;
-    
-    hour = hour ? hour : 12;
-    //HOURS EQUALS HOURS 
-    
-    return hour + " " + ampm;
-  };
-  //INITIALIZE FORMATAMPM(HOURS)
-  formatAMPM();
-  /////////////////////////////////////////////////////////////////////
-  /////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////
+//CREATE A FUNCTION TO FORMAT HOURS && CHANGES THE HOUR DISPLAY FROM MIILTARY TIME TO REGULAR TIME
+/////////////////////////////////////////////////////////////////////
+function formatAMPM(hour) {
+  console.log("format AMPM is working!!");
+
+  const ampm = hour >= 12 ? "PM" : "AM";
+  hour = hour % 12;
+
+  hour = hour ? hour : 12;
+  //HOURS EQUALS HOURS 
+
+  return hour + " " + ampm;
+};
+//INITIALIZE FORMATAMPM(HOURS)
+formatAMPM();
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////////////////
+//CREATES HOURLY WEATHER CARDS
+/////////////////////////////////////////////////////////////////////
 function displayWeatherTimeIcons() {
 
   //THIS VARIBALE STORES THE HOUR ID
   const hourId = parseInt(moment().hour());
-  console.log("hourId:", hourId);
+  // console.log("hourId:", hourId);
 
   //HOUR FOR LOOP 
   for (let hour = 0; hour <= 24; hour++) {
     //variable hour = 9am; less than 6pm; hour increase each time)
 
     const hourEl = moment().hour(hour);
-    console.log("hourEl:", hourEl)
+    // console.log("hourEl:", hourEl)
 
     const hourIndex = parseInt(hourEl.format("h"));
-    console.log("hourIndex:", hourIndex);
+    // console.log("hourIndex:", hourIndex);
 
     //CREATE A ROW TO WRAP EVERYTHING IN
     const card = $('<div class="card"></div>');
 
     const cardHeader = $('<div class="cardTitle"></div>');
 
-    const hourSpan = $('<span class="hour" style="color:#ffffff;font-size:15px;font-weight: bold;font-family:Poppins, sans-serif;">' + formatAMPM(hour) + "</span>");
-    console.log("hour:", hour);
+    const hourSpan = $(
+      '<span class="hour" style="color:#182978;font-size:15px;font-weight:bolder;font-family:Poppins, sans-serif;">' +
+        formatAMPM(hour) +
+        "</span>"
+    );
+    // console.log("hour:", hour);
 
     cardHeader.append(hourSpan);
 
@@ -154,10 +279,10 @@ function displayWeatherTimeIcons() {
     const iconSpan = $('<span class="icon"></span>');
 
     const iconId = "icon-temp-" + hour;
-    console.log("iconId:", iconId);
+    // console.log("iconId:", iconId);
 
     iconSpan.attr("id", iconId);
-    console.log("iconSpan:", iconSpan);
+    // console.log("iconSpan:", iconSpan);
 
     cardBody.append(iconSpan);
 
@@ -167,7 +292,7 @@ function displayWeatherTimeIcons() {
     // console.log("temp:", temp);
 
     timeTempSpan.attr("data-iconspan", hour);
-    console.log(cardFooter);
+    // console.log(cardFooter);
 
     cardFooter.append(timeTempSpan);
 
@@ -178,5 +303,5 @@ function displayWeatherTimeIcons() {
     $(".horizontal-scroll").append(card);
   };
 };
-
-
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
